a2584de66ed520f0f1a7839913fa4da039f42f91
Luis Angel Peña <lapena@mavi.mx>
2021-03-16 16:30:59 -0600

    feat:: Add the structure of the proyect

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+module.exports = {
+    entry: './src/app/app.module',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "src"),
+        compress: true,
+        port: 9000,
+        publicPath: "/dist/"
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.html$/,
+                use: 'raw-loader'
+            }
+
+        ]
+    }
+};
